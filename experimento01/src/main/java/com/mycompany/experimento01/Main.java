cd0b459e79f55ae8f82a80d368091ed7553befb9
Gaston Sabugo <gastons_92@hotmail.com>
2020-11-21 22:45:06 -0300

    agregados vsl e imagenes

--- experimento01/src/main/java/com/mycompany/experimento01/Main.java
+++ experimento01/src/main/java/com/mycompany/experimento01/Main.java
@@ -1,5 +1,6 @@
 package com.mycompany.experimento01;
 import static spark.Spark.get;
+import static spark.Spark.staticFiles;
 
 /**
  *
@@ -9,6 +10,9 @@ public class Main {
     
     public static void main(String[] args){
         //consulta -> http://localhost:4567/getImg
+        // http://localhost:4567/getImg?indice=1
+        
+        staticFiles.location("/public");
 
         get("/getImg",Controlador.getImg);
     }
