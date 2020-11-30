87a66211b533520215d1fc9f619fe6e5fc49654a
Luciano <ljcanales1404@gmail.com>
2020-11-30 01:39:13 -0300

    lombok 1.18.8 - con esta version anda!! (BUILD)

--- test_diario/src/main/java/com/mycompany/test_diario/Noticia.java
+++ test_diario/src/main/java/com/mycompany/test_diario/Noticia.java
@@ -1,46 +1,23 @@
 package com.mycompany.test_diario;
 
+import lombok.Data;
+
 
 /**
  *
  * @author Gaston
  */
 
-
+@Data
 public class Noticia {
-    private String titulo;
-    private String urlImg;
+    private String tit;
+    private String url;
     private String genero;
-    private String descripcion;
+    private String desc;
     
     public Noticia(String t, String url){
-        titulo = t;
-        urlImg = url;
+        this.tit = t;
+        this.url = url;
     }
-    
-    public void setTit(String s){
-        titulo = s;
-    }
-    public void setUrl(String s){
-        urlImg = s;
-    }
-    public void setGen(String s){
-        genero = s;
-    }
-    public void setDesc(String s){
-        descripcion = s;
-    }
-    
-    public String getTit(){
-        return titulo;
-    }
-    public String getUrl(){
-        return urlImg;
-    }
-    public String getGen(){
-        return genero;
-    }
-    public String getDesc(){
-        return descripcion;
-    }
+
 }
